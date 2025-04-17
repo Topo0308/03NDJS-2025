1438f52084b23ec3bee18b8c339d9acf026622c8
Topo0308 <Topo0308>
2025-04-17 13:06:51 +0200

    Middleware d'authentification JWT

---
+++ Day-4/project-root/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');  //librairie qui permet de créer et vérifier des tokens 
+const SECRET = 'supersecret' //signer et vérifier les tokens
+
+function authMiddleware(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(401).json({ error: 'Missing token' });
+
+  const token = authHeader.split(' ')[1];
+  try {
+    const decoded = jwt.verify(token, SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+}
+
+module.exports = { authMiddleware, SECRET };
