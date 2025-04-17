f5487004f07b35b7214f9a069cdb66defa62f463
Topo0308 <Topo0308>
2025-04-17 22:32:22 +0200

    modification pour connecter Mongodb

--- Day-4/project-root/server.js
+++ Day-4/project-root/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
 
 app.use(express.json());
 app.use('/api', userRoutes); // Routes
+
+mongoose.connect('mongodb://localhost:27017/auth_api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
